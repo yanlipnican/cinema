e99af8093f9aab88c1b7733b5078d5705d5ebf80
Jan Lipnican <jan.lipnican@cognito.cz>
2016-08-21 13:38:13 +0200

    Choped middlewares to functions

--- src/backend/middlewares.js
+++ src/backend/middlewares.js
@@ -6,24 +6,29 @@ export default (app) => {
         next();
     })
 
-    // admin middleware for basic data
-    app.use('/admin', (req, res, next) => {
+    app.use('/admin', authAdmin);
+
+    app.use('/admin', getAccessableCollections);
+
+    // admin middleware for basic data and session handling
+    function authAdmin(req, res, next) {
         if (req.session._id || req._parsedUrl.path === "/admin/login" || req._parsedUrl.path === "/admin/register"){
-            models.adminuser.findOne({ _id: req.session._id }, (err, admin) => {        
-                
+            models.adminuser.findOne({ _id: req.session._id }, (err, admin) => {                        
                 req._cms_data.admin = admin;
-                req._cms_data.cols = [];
-
-                for (let key in models) {
-                    if (models[key].access) req._cms_data.cols.push(models[key]._name);
-                }
-
                 next();
             });
         }
         else {
             res.redirect('/admin/login');
         }
-	});
+	};
+
+    function getAccessableCollections(req, res, next){
+        req._cms_data.cols = [];
+        for (let key in models) {
+            if (models[key].access) req._cms_data.cols.push(models[key]._name);
+        }
+        next();
+    }
 
 };
